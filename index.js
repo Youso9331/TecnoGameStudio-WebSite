e4cd8c4a72c37fc262c71f6ebceec9fc382637e8
Youso9331 <angladematheo@gmail.com>
2026-08-28 17:36:44 +0200

    New

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/** 
-const barre = document.querySelector('.barre');
+
+/**const barre = document.querySelector('.barre');
 let dernierScroll = 0;
 const barre = document.querySelector('.barre');
 
@@ -15,3 +15,23 @@ window.addEventListener('scroll', function() {
     dernierScroll = scrollActuel;
 });
 */
+
+const images = ["image1.png", "image2.png", "image3.png"];
+
+let indexActuel = 0;
+
+const imageElement = document.getElementById("image-carousel");
+
+function mettreAJourImage() {
+    imageElement.src = images[indexActuel];
+}
+
+document.getElementById("fleche-droite").addEventListener("click", () => {
+    indexActuel = (indexActuel + 1) % images.length;
+    mettreAJourImage();
+});
+
+document.getElementById("fleche-gauche").addEventListener("click", () => {
+    indexActuel = (indexActuel - 1 + images.length) % images.length;
+    mettreAJourImage();
+});
