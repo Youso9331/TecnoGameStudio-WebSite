d32866c46de5870028375468a83a0a9dceabb6cf
Youso9331 <angladematheo@gmail.com>
2026-08-31 20:21:00 +0200

    New

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-
-const barre = document.querySelector('.barre');
-let dernierScroll = 0;
-const barre = document.querySelector('.barre');
-
-window.addEventListener('scroll', function() {
-    const scrollActuel = window.scrollY;
-
-    if (scrollActuel > dernierScroll && scrollActuel > 100) {
-        barre.classList.add('cachee');
-    } else {
-        barre.classList.remove('cachee');
-    }
-
-    dernierScroll = scrollActuel;
-});
-
-
 const images = ["image1.png", "image2.png", "image3.png"];
 
 let indexActuel = 0;
